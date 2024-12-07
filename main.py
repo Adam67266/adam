63b7e9e480bac16dc85c87744e1c2e50af0bbaaa
Adam67266 <vladadamchuch@gmail.com>
2024-12-07 14:34:39 +0200

    ttt

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world")
+import random
+import time
+
+class Pet:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.age = 0
+        self.hunger = 50 
+        self.energy = 50  
+        self.happiness = 50  
+
+    
